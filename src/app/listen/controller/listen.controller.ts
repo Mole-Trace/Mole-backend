4322b31d0be17a4b14b11c3c0b35762a90996c7c
ali <mareafati110@gmail.com>
2021-08-08 16:16:04 +0430

    wip

--- src/app/listen/controller/listen.controller.ts
+++ src/app/listen/controller/listen.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller()
-export class ListenController {}
+@Controller('listen')
+export class ListenController {
+  @Get()
+  async test() {
+    return 'ok';
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      url: 'redis://localhost:6379',
+    },
+  });
+
+  await app.startAllMicroservices();
+
+  await app.listen(3500);
 }
 bootstrap();
