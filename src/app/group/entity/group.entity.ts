6788c5be25619f3483bb6ec98a8cca82ac659a19
ali <mareafati110@gmail.com>
2021-08-08 22:24:42 +0430

    relation user to group entity

--- src/app/group/entity/group.entity.ts
+++ src/app/group/entity/group.entity.ts
@@ -1,11 +1,15 @@
 import { BasicEntity } from '../../../common/basic/basic';
 import { Event } from '../../event/entity/event.entity';
-import { Column, Entity, OneToMany } from 'typeorm';
+import { User } from '../../user/entity/user.entity';
+import { Column, Entity, ManyToMany, OneToMany } from 'typeorm';
 
 @Entity()
 export class Group extends BasicEntity {
   @Column()
   name: string;
+
+  @ManyToMany(() => User)
+  user: User[];
 
   @OneToMany(() => Event, (event) => event.group, { onDelete: 'CASCADE' })
   events: Event[];
