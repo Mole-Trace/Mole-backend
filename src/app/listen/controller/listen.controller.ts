bcd7ab0df01d918a0db5b4ac5834e0ca50531fdb
ali <mareafati110@gmail.com>
2021-08-08 21:54:58 +0430

    add listen module

--- src/app/listen/controller/listen.controller.ts
+++ src/app/listen/controller/listen.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Post } from '@nestjs/common';
+import {
+  MessagePattern,
+  Payload,
+  Ctx,
+  RedisContext,
+  ClientProxy,
+  EventPattern,
+} from '@nestjs/microservices';
 
 @Controller('listen')
 export class ListenController {
-  @Get()
+  constructor(@Inject('REDIS_SERVICE') private client: ClientProxy) {}
+
+  @Post()
   async test() {
+    this.client.emit('notifications', 'ali');
     return 'ok';
+  }
+
+  @EventPattern('notifications')
+  getNotifications(@Payload() data: any) {
+    // console.log(`Channel: ${context.getChannel()}`);
+    console.log('ali marefati', data);
   }
 }
--- src/app/listen/listen.module.ts
+++ src/app/listen/listen.module.ts
@@ -1,7 +1,19 @@
 import { ListenController } from './controller/listen.controller';
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'REDIS_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          url: 'redis://localhost:6379',
+        },
+      },
+    ]),
+  ],
   controllers: [ListenController],
 })
 export class ListenModule {}
