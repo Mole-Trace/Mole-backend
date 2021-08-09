b2a49528ecd900817f09ecc304af37ebe49c1346
ali <mareafati110@gmail.com>
2021-08-09 18:56:16 +0430

    bugfix

--- src/app/memdb/memdb.module.ts
+++ src/app/memdb/memdb.module.ts
@@ -1,8 +1,15 @@
 import { MemdbService } from './service/memdb.service';
-import { Global, Module } from '@nestjs/common';
+import { CacheModule, Global, Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Global()
 @Module({
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      url: 'redis://localhost:6379',
+    }),
+  ],
   providers: [MemdbService],
   exports: [MemdbService],
 })
