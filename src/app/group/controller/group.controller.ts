066f53f4e5e4fab8afb8edb8bc0d717bb70ea88f
ali <mareafati110@gmail.com>
2021-08-10 17:35:44 +0430

    create new group

--- src/app/group/controller/group.controller.ts
+++ src/app/group/controller/group.controller.ts
@@ -3,6 +3,7 @@ import { Authenticate } from '../../../common/decorators/auth.decorator';
 import { GetUser } from '../../auth/decorators/get-user.decorator';
 import { User } from '../../user/entity/user.entity';
 import { AddNewGroupBodyDto } from '../dto/add-group.dto';
+import { Group } from '../entity/group.entity';
 import { GroupService } from '../service/group.service';
 import { Body, Post } from '@nestjs/common';
 
@@ -12,7 +13,10 @@ export class GroupController {
 
   @Authenticate()
   @Post('add')
-  async addNewGroup(@GetUser() user: User, @Body() body: AddNewGroupBodyDto) {
+  async addNewGroup(
+    @GetUser() user: User,
+    @Body() body: AddNewGroupBodyDto,
+  ): Promise<Group> {
     const group = await this.groupService.createNewGroup(user, body);
     return group;
   }
